7f66cf79bb1ad19bb1325937a1af798538fad380
Soundarya Balamuthiah <sbalamuthiah@horizon.csueastbay.edu>
2017-12-23 14:00:32 -0800

    commit

---
+++ datatypes.ts
@@ -0,0 +1,12 @@
+let fname: string = 'Soundarya';
+let age: number = 23;
+let isValid : boolean = false;
+console.log(isValid);
+let dob: Date = new Date('10-oct-1993');
+console.log(dob);
+
+let unionTypes : string | number;
+unionTypes='terror';
+console.log(unionTypes);
+unionTypes=50;
+console.log(unionTypes);
